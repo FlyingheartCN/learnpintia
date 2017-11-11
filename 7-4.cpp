94bbfdc1713b7c9888362270af41bb3cb4e07346
F Feng <30972724+FlyingheartCN@users.noreply.github.com>
2017-11-11 21:18:06 +0800

    Update 7-4.cpp

--- 7-4.cpp
+++ 7-4.cpp
@@ -1,43 +1,92 @@
 #include <iostream>
 using namespace std;
+/*全局变量代表对比的结果，每次对比开始之前会被重设为1；*/
+int CheckResult;
 
-//构建一个二叉树
-int same;
+/*定义一个结构体，作为二叉树的一个节点*/
 typedef struct SearchBTree
 {
 	int val;
 	struct SearchBTree *left, *right;
 }SearchBTree, *sTree;
 
-void init_tree(sTree &root);
-void creat_Tree(sTree &root, int NVal);
-void checksame(sTree &NTree, sTree &LTree);
+/*用于初始化二叉树*/
+void TreeInit(sTree &root) {
+	root = NULL;
+}
+/*将制定的NVal插入*/
+void TreeInsert(sTree &root,int InsertValue) {
+	if (root == NULL) {
+		root = (sTree)malloc(sizeof(SearchBTree));
+		root->val = InsertValue;
+		root->left = NULL;
+		root->left = NULL;
+	}
+	else {
+		if (InsertValue<root->val) {
+			TreeInsert(root->left, InsertValue);
+		}
+		if (InsertValue>root->val) {
+			TreeInsert(root->right, InsertValue);
+		}
+	}
+}
+/*检查传入的的两个二叉树，如果有不同则把CheckResult设为1*/
+void CheckSame(sTree &NTree, sTree &LTree) {
+	if (NTree->val == LTree->val)
+	{
+		if (NTree->left != NULL && LTree->left != NULL) {
+			CheckSame(NTree->left, LTree->left);
+		}
+		if (NTree->left != NULL && LTree->left == NULL) {
+			CheckResult = 0;
+		}
+		if (NTree->left == NULL && LTree->left != NULL) {
+			CheckResult = 0;
+		}
+		/*三种情况，两个都是NULL，两个都不是NULL，两个有一个NULL
+		第一种是相同的，第二种继续递归查找，第三种不同直接设为0*/
+		if (NTree->right != NULL && LTree->right != NULL) {
+			CheckSame(NTree->right, LTree->right);
+		}
+		if (NTree->right != NULL && LTree->right == NULL) {
+			CheckResult = 0;
+		}
+		if (NTree->right == NULL && LTree->right != NULL) {
+			CheckResult = 0;
+		}
+	}
+	else { CheckResult = 0; }
+}
 
 int main() {
-	int N, L,NVal;
+	int N, L, NVal, LVal;
 	sTree NTree;
 	sTree LTree;
 	while (true)
 	{
 		cin >> N;
-		if (N == 0) { break; }//如果发现是0就退出循环
+		if (N == 0) { break; }
 		cin >> L;
-		//读取第一行并存储在NTree中
-		init_tree(NTree);
+		/*初始化NTree用来接收被对比二叉树*/
+		TreeInit(NTree);
 		for (int i = 0; i < N; i++) {
 			cin >> NVal;
-			creat_Tree(NTree,NVal);
+			TreeInsert(NTree, NVal);
 		}
-		//逐行读取并存储在LTree中
+		/*开始循环，每次循环做一次对比*/
 		for (int i = 0; i < L; i++) {
-			init_tree(LTree);
+			/*初始化LTree并接受二叉树*/
+			TreeInit(LTree);
 			for (int i = 0; i < N; i++) {
-				cin >> NVal;
-				creat_Tree(LTree,NVal);
+				cin >> LVal;
+				TreeInsert(LTree, LVal);
 			}
-			same = 1;
-			checksame(NTree, LTree);
-			if (same) {
+			/*对比*/
+			CheckResult = 1;
+			CheckSame(NTree, LTree);
+			/*对比结果*/
+			if (CheckResult) {
 				cout << "Yes" << endl;
 			}
 			else {
@@ -46,43 +95,4 @@ int main() {
 		}
 	}
 	return 0;
-}
-void init_tree(sTree &root) {
-	root = NULL;
-}
-
-void creat_Tree(sTree &root,int NVal) {
-	if (root == NULL) {
-		root = (sTree)malloc(sizeof(SearchBTree));
-		root->val = NVal;
-		root->left = NULL;
-		root->left = NULL;
-	}
-	else {
-		if (NVal<root->val) {
-			creat_Tree(root->left, NVal);
-		}
-		if (NVal>root->val) {
-			creat_Tree(root->right, NVal);
-		}
-	}
-}
-
-void checksame(sTree &NTree, sTree &LTree) {
-	if (NTree->val == LTree->val)
-	{
-		if (NTree->left != NULL && LTree->left != NULL) {
-			checksame(NTree->left, LTree->left);
-		}
-		if (NTree->left != NULL && LTree->left == NULL) {
-			same = 0;
-		}
-		if (NTree->left == NULL && LTree->left != NULL) {
-			same = 0;
-		}
-		if (NTree->right != NULL && LTree->right != NULL) {
-			checksame(NTree->right, LTree->right);
-		}
-	}
-	else { same = 0; }
 }
